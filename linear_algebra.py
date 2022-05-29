a6a897905b30817a6b668463aa769fe7dcd1b5f9
djccnt15 <djccnt15@gmail.com>
2022-05-29 21:48:00 +0900

    update linear_algebra

--- linear_algebra.py
+++ linear_algebra.py
@@ -225,7 +225,7 @@ def householder(v):
     return H
 
 # creating augmented matrix
-def aug_mat(a, b):
+def mat_aug(a, b):
     x = copy.deepcopy(a)
 
     x = [x + [b[i]] for i, x in enumerate(x)]
@@ -233,7 +233,7 @@ def aug_mat(a, b):
     return x
 
 # separating coefficient matrix
-def coef_mat(a):
+def mat_coef(a):
     n = len(a)
 
     x = [a[i][:-1] for i in range(n)]
@@ -242,37 +242,16 @@ def coef_mat(a):
     return x, y
 
 # pivoting augmented matrix
-def pivot_mat(a, b):
-    mat = aug_mat(a, b)
+def mat_pivot(mat):
 
     mat = sorted(mat, key=lambda x: abs(x[0]), reverse=True)
 
     return mat
 
-# Gauss-Jordan elimination
-def gauss_jordan_eli(a, b):
-    mat = pivot_mat(a, b)
-    n = len(mat)
-
-    for i in range(n):
-        mat[i] = [ele / mat[i][i] for ele in mat[i]]
-
-        for j in range(n):
-            if i == j:
-                continue
-
-            mat_tmp = [ele * -mat[j][i] for ele in mat[i]]
-
-            for k in range(len(mat[i])):
-                mat[j][k] += mat_tmp[k]
-
-    x, y = coef_mat(mat)
-
-    return y
-
 # Gauss elimination
 def gauss_eli(a, b):
-    mat = pivot_mat(a, b)
+    mat = mat_aug(a, b)
+    mat = mat_pivot(mat)
     n = len(mat)
 
     # gauss elimination
@@ -288,32 +267,89 @@ def gauss_eli(a, b):
             mat[i][n] = mat[i][n] - mat[i][k] * mat[k][n]
         mat[i][n] /= mat[i][i]
 
-    x, y = coef_mat(mat)
+    x, y = mat_coef(mat)
 
     return y
 
-# cofactor expansion
-def det_rec(a):
-    n = len(a)
-    res = 0
-
-    if n == 2:
-        res = a[0][0] * a[1][1] - a[1][0] * a[0][1]
-
-        return res
+# Gauss-Jordan elimination
+def gauss_jordan_eli(a):
+    mat = copy.deepcopy(a)
+    n = len(mat)
 
     for i in range(n):
-        x = copy.deepcopy(a)
-        x = x[1:]
-        nx = len(x)
+        mat[i] = [ele / mat[i][i] for ele in mat[i]]
 
-        for j in range(nx):
-            x[j] = x[j][0:i] + x[j][i+1:]
+        for j in range(n):
+            if i == j:
+                continue
 
-        sign = (-1) ** (i % 2)
-        res += sign * a[0][i] * det_rec(x)
+            mat_tmp = [ele * -mat[j][i] for ele in mat[i]]
+
+            for k in range(len(mat[i])):
+                mat[j][k] += mat_tmp[k]
+
+    return mat
+
+# solve equation with Gauss-Jordan elimination
+def solve(a, b):
+    mat = mat_aug(a, b)
+    mat = mat_pivot(mat)
+    mat = gauss_jordan_eli(mat)
+    x, y = mat_coef(mat)
+
+    return y
+
+# creating matrix augmented matrix
+def mat_aug_inv(a, b):
+    x = copy.deepcopy(a)
+
+    x = [x + b[i] for i, x in enumerate(x)]
+
+    return x
+
+# separating coefficient matrix
+def mat_coef_inv(a, b):
+    n = len(a)
+
+    x = [a[i][:b] for i in range(n)]
+    y = [[y for y in a[i][b:]] for i in range(n)]
+
+    return x, y
+
+# inverse matrix
+def mat_inv(a):
+    n = len(a)
+    i = mat_identity(n)
+    mat = mat_aug_inv(a, i)
+    mat = mat_pivot(mat)
+    mat = gauss_jordan_eli(mat)
+    x, res = mat_coef_inv(mat, n)
 
     return res
+
+# # cofactor expansion
+# def det_rec(a):
+#     n = len(a)
+#     res = 0
+
+#     if n == 2:
+#         res = a[0][0] * a[1][1] - a[1][0] * a[0][1]
+
+#         return res
+
+#     else:
+#         for i in range(n):
+#             x = copy.deepcopy(a)
+#             x = x[1:]
+#             nx = len(x)
+
+#             for j in range(nx):
+#                 x[j] = x[j][0:i] + x[j][i+1:]
+
+#             sign = (-1) ** i
+#             res += sign * a[0][i] * det_rec(x)
+
+#         return res
 
 if __name__ == "__main__":
     a = [1, 2, 3]
@@ -377,12 +413,14 @@ if __name__ == "__main__":
     y = [4, 5, 3]
     print(f'\nx = {x}\ny = {y}')
 
-    print(f'\ncreating augmented matrix: {aug_mat(x, y)}')
-    print(f'\nsorting augmented matrix: {pivot_mat(x, y)}')
-    print(f'\nGauss Jordan elimination: {gauss_jordan_eli(x, y)}')
+    print(f'\ncreating augmented matrix: {mat_aug(x, y)}')
+    print(f'\nsorting augmented matrix: {mat_pivot(mat_aug(x, y))}')
     print(f'\nGauss elimination: {gauss_eli(x, y)}')
+    print(f'\nGauss Jordan elimination: {gauss_jordan_eli(mat_aug(x, y))}')
+    print(f'\nSolve equation with Gauss-Jordan elimination: {solve(x, y)}')
+    print(f'\ninverse matrix: {mat_inv(x)}')
 
-    j = [[5, 2,-3, 4], [5, 9, 7, 8], [11, 10, 6, 12], [13, 14, 15, 16]]
-    print(f'\nj = {j}')
+    # j = [[5, 2, -3, 4], [5, 9, 7, 8], [11, 10, 6, 12], [13, 14, 15, 16]]
+    # print(f'\nj = {j}')
 
-    print(f'\ndet(A): {det_rec(j)}')
+    # print(f'\ndet(A): {det_rec(j)}')
