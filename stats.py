06166696b034237558679dea7616fec9aa6ddb61
djccnt15 <djccnt15@gmail.com>
2023-01-02 21:31:43 +0900

    update type hint to aliased form

--- stats.py
+++ stats.py
@@ -1,18 +1,21 @@
-def freq(data: list) -> dict:
+numeric = list[int | float]
+
+
+def freq(data: numeric) -> dict:
     """returns frequency of each value"""
 
     res = {val: data.count(val) for val in set(data)}
     return res
 
 
-def freq_rel(data: list) -> dict:
+def freq_rel(data: numeric) -> dict:
     """returns relative frequency of each value"""
 
     res = {val: data.count(val) / len(data) for val in set(data)}
     return res
 
 
-def freq_cum(data: list) -> dict:
+def freq_cum(data: numeric) -> dict:
     """returns cumulative frequency of sorted value"""
 
     data_sort = sorted(list(set(data)))
@@ -20,14 +23,14 @@ def freq_cum(data: list) -> dict:
     return res
 
 
-def bar(data: list) -> float:
+def bar(data: numeric) -> float:
     """returns expectation/sample mean"""
 
     res = sum(data) / len(data)
     return res
 
 
-def devi(data: list) -> dict:
+def devi(data: numeric) -> dict:
     """returns deviation of each value"""
 
     b = bar(data)
@@ -35,14 +38,14 @@ def devi(data: list) -> dict:
     return res
 
 
-def mean_weight(data: list, weights: list) -> float:
+def mean_weight(data: numeric, weights: numeric) -> float:
     """returns weighted mean"""
 
     res = sum(v * w for v, w in zip(data, weights)) / sum(weights)
     return res
 
 
-def prod_for(data: list) -> float:
+def prod_for(data: numeric) -> float:
     """product all elements in data with for loop"""
 
     res = 1
@@ -51,34 +54,34 @@ def prod_for(data: list) -> float:
     return res
 
 
-def prod_rec(data: list) -> float:
+def prod_rec(data: numeric) -> float:
     """product all elements in data with recursion"""
 
     return data[0] if len(data) == 1 else data[0] * prod_rec(data[1:])
 
 
-def mean_geom(data: list) -> float:
+def mean_geom(data: numeric) -> float:
     """returns geometric mean of data"""
 
     res = prod_rec(data) ** (1 / len(data))
     return res
 
 
-def mean_harm(data: list) -> float:
+def mean_harm(data: numeric) -> float:
     """returns harmonic mean of data"""
 
     res = len(data) / (sum(1 / v for v in data))
     return res
 
 
-def mean_trimmed(data: list, k: int) -> float:
+def mean_trimmed(data: numeric, k: int) -> float:
     """return trimmed mean from data, k defines number of data to trim"""
 
     res = bar(sorted(data)[k:-k])
     return res
 
 
-def median(data: list) -> float:
+def median(data: numeric) -> float:
     """returns median number of data"""
 
     data = sorted(data)
@@ -92,7 +95,7 @@ def median(data: list) -> float:
         return data[i]
 
 
-def mode(data: list) -> list | None:
+def mode(data: numeric) -> list | None:
     """returns mode value from data"""
 
     cnt = {v: data.count(v) for v in set(data)}
@@ -104,14 +107,14 @@ def mode(data: list) -> list | None:
         return res
 
 
-def data_range(data: list) -> float:
+def data_range(data: numeric) -> float:
     """returns range of data"""
 
     res = max(data) - min(data)
     return res
 
 
-def quantile(data: list, q: float) -> float:
+def quantile(data: numeric, q: float) -> float:
     """returns quantile value from data"""
 
     data = sorted(data)
@@ -122,14 +125,14 @@ def quantile(data: list, q: float) -> float:
     return res
 
 
-def iqr_range(data: list) -> float:
+def iqr_range(data: numeric) -> float:
     """returns IQR range from data"""
 
     res = quantile(data, 0.75) - quantile(data, 0.25)
     return res
 
 
-def var(data: list, dof: int = 0) -> float:
+def var(data: numeric, dof: int = 1) -> float:
     """returns variance of data"""
 
     b = bar(data)
@@ -137,7 +140,7 @@ def var(data: list, dof: int = 0) -> float:
     return res
 
 
-def std(data: list, dof: int = 0) -> float:
+def std(data: numeric, dof: int = 1) -> float:
     """return standard deviation of data"""
 
     res = var(data, dof) ** (1/2)
@@ -150,30 +153,30 @@ def standardize(num: float, bar: float, std: float) -> float:
     return (num - bar) / std
 
 
-def scaler_standard(data: list) -> list:
+def scaler_standard(data: numeric, dof: int = 0) -> list:
     """returns standardized values of data"""
 
-    b, s = bar(data), std(data)
+    b, s = bar(data), std(data, dof)
     res = [standardize(d, b, s) for d in data]
     return res
 
 
-def variation(data: list) -> float:
+def variation(data: numeric, dof: int = 0) -> float:
     """returns coefficient of variation"""
 
-    res = std(data) / bar(data)
+    res = std(data, dof) / bar(data)
     return res
 
 
-def skew(data: list, dof: int = 0) -> float:
+def skew(data: numeric, dof: int = 0) -> float:
     """returns skewness of data"""
 
-    b, s = bar(data), std(data)
+    b, s = bar(data), std(data, dof)
     res = sum(standardize(d, b, s) ** 3 for d in data) / (len(data) - dof)
     return res
 
 
-def skew_adv(data: list) -> float:
+def skew_adv(data: numeric) -> float:
     """returns fixed skewness of data"""
 
     n = len(data)
@@ -181,22 +184,22 @@ def skew_adv(data: list) -> float:
     return res
 
 
-def kurtosis(data: list, dof: int = 0) -> float:
+def kurtosis(data: numeric, dof: int = 0) -> float:
     """returns kurtosis of data"""
 
-    b, s = bar(data), std(data)
+    b, s = bar(data), std(data, dof)
     res = (sum(standardize(d, b, s) ** 4 for d in data) / (len(data) - dof))
     return res
 
 
-def kurtosis_norm(data: list, dof: int = 0) -> float:
+def kurtosis_norm(data: numeric, dof: int = 0) -> float:
     """returns kurtosis of normal distributed data"""
 
     res = kurtosis(data, dof) - 3
     return res
 
 
-def kurtosis_adv(data: list) -> float:
+def kurtosis_adv(data: numeric) -> float:
     """returns fixed kurtosis of data"""
 
     k, n = kurtosis(data, 1), len(data)
@@ -204,14 +207,14 @@ def kurtosis_adv(data: list) -> float:
     return res
 
 
-def jarque_bera(data: list, dof: int = 0) -> float:
-    """returns Jarque-Bera normailty test value"""
+def jarque_bera(data: numeric, dof: int = 0) -> float:
+    """returns Jarque-Bera normality test value"""
 
     res = (len(data) / 6) * (skew(data, dof) ** 2 + ((kurtosis_norm(data, dof) ** 2) / 4))
     return res
 
 
-def cov(data_a: list, data_b: list, dof: int = 0) -> float:
+def cov(data_a: numeric, data_b: numeric, dof: int = 1) -> float:
     """returns covariance of two random variables"""
 
     b_a, b_b = bar(data_a), bar(data_b)
@@ -219,16 +222,16 @@ def cov(data_a: list, data_b: list, dof: int = 0) -> float:
     return res
 
 
-def pearson(data_a: list, data_b: list, dof: int = 0) -> float:
+def pearson(data_a: numeric, data_b: numeric, dof: int = 0) -> float:
     """returns Pearson correlation coefficient of two data"""
 
-    b_a, s_a = bar(data_a), std(data_a)
-    b_b, s_b = bar(data_b), std(data_b)
+    b_a, s_a = bar(data_a), std(data_a, dof)
+    b_b, s_b = bar(data_b), std(data_b, dof)
     res = sum(standardize(a, b_a, s_a) * standardize(b, b_b, s_b) for a, b in zip(data_a, data_b)) / len(data_a) - dof
     return res
 
 
-def corrcoef(a: list, b: list) -> float:
+def corrcoef(a: numeric, b: numeric) -> float:
     """returns Pearson's r of two data"""
 
     res = cov(a, b) / ((cov(a, a) * cov(b, b)) ** 0.5)
@@ -271,7 +274,7 @@ def multiset(n: int, k: int) -> float:
     return res
 
 
-def lineFit(x: list, y: list) -> tuple:
+def lineFit(x: numeric, y: numeric) -> tuple:
     """returns linear regression coefficient(weight) and intercept of two random variables"""
 
     x_bar, y_bar = bar(x), bar(y)
@@ -283,20 +286,20 @@ def lineFit(x: list, y: list) -> tuple:
 
 
 if __name__ == "__main__":
-    a = [1, 2, 1, 3, 1, 2, 3, 1, 1, 2, 1, 1]
+    a: numeric = [1, 2, 1, 3, 1, 2, 3, 1, 1, 2, 1, 1]
     print(f'\n{a=}\n')
 
     print(f'frequency of a: {freq(a)}')
     print(f'relative frequency of a: {freq_rel(a)}')
     print(f'cumulative frequency of a: {freq_cum(a)}')
 
-    b = [2, 3, 4, 5, 6, 7, 1, 2, 3, 4]
+    b: numeric = [2, 3, 4, 5, 6, 7, 1, 2, 3, 4]
     print(f'\n{b=}\n')
 
     print(f'deviation of each value of b: {devi(b)}')
 
-    c = [1, 2, 3, 4, 5]
-    d = [5, 4, 3, 2, 1]
+    c: numeric = [1, 2, 3, 4, 5]
+    d: numeric = [5, 4, 3, 2, 1]
     print(f'\n{c=}\n{d=}\n')
 
     print(f'weighted mean of c as data, d as weight: {mean_weight(c, d)}')
@@ -306,8 +309,8 @@ if __name__ == "__main__":
     print(f'harmonic mean of c: {mean_harm(c)}')
     print(f'trimmed mean of c, k=1: {mean_trimmed(c, 1)}')
 
-    c = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4]
-    d = [1, 2, 3, 4, 5]
+    c: numeric = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4]
+    d: numeric = [1, 2, 3, 4, 5]
     print(f'\n{c=}\n{d=}\n')
 
     print(f'median of c: {median(c)}')
@@ -315,8 +318,8 @@ if __name__ == "__main__":
     print(f'mode of c: {mode(c)}')
     print(f'mode of d: {mode(d)}')
 
-    e = [-5, -2, 1, 2, 3, 4, 5, 6, 7, 8, 11, 13, 15, 17, 19, 25, 87, 99, 100]
-    q = [0.25, 0.5, 0.75]
+    e: numeric = [-5, -2, 1, 2, 3, 4, 5, 6, 7, 8, 11, 13, 15, 17, 19, 25, 87, 99, 100]
+    q: numeric = [0.25, 0.5, 0.75]
     print(f'\n{e=}\n{q=}\n')
 
     print(f'Q1, Q2, Q3 of e: {[quantile(e, q) for q in q]}')
@@ -328,8 +331,8 @@ if __name__ == "__main__":
     print(f'skewness of e: {skew(e, 1)}')
     print(f'kurtosis of e: {kurtosis_norm(e, 1)}')
 
-    f = [2.23, 4.78, 7.21, 9.37, 11.64, 14.23, 16.55, 18.70, 21.05, 23.21]
-    g = [139, 123, 115, 96, 62, 54, 10, -3, -13, -55]
+    f: numeric = [2.23, 4.78, 7.21, 9.37, 11.64, 14.23, 16.55, 18.70, 21.05, 23.21]
+    g: numeric = [139, 123, 115, 96, 62, 54, 10, -3, -13, -55]
     print(f'\n{c=}\n{g=}\n')
 
     print(f'cov of a, b: {cov(f, g)}')
